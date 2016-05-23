c599ce73015e46540db92b35d57a97723bf88932
tergog <tergog@mail.ru>
2016-05-24 02:19:38 +0300

    Final version

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 (function () {
+
+    var customData = {
+        caretImg: ['./img/caret_down.png', './img/caret_up.png'],
+        classes: {
+            hidden: ' hidden_block',
+            opacity: ' hidden_opacity',
+            contactUser: ' contact_user',
+            smileImg: ' smile_img',
+            active: ' active'
+        }
+    };
+
     document.addEventListener("DOMContentLoaded", function () {
+
         var menu = document.getElementById("menu_bar");
-        var menuNavigation = document.getElementById('menu_navigation');
-        var textarea = document.getElementsByTagName('textarea')[0];
-        var children = menuNavigation.children;
+        var navMnu = document.getElementById('menu_navigation');
+        var navMnuChildren = navMnu.children;
+        var textarea = document.getElementById('textarea');
         var chat = document.getElementsByClassName('chat_window')[0];
         var chatWindow = document.getElementsByClassName('chat_list_style')[0];
         var contactList = document.getElementsByClassName('contact_list')[0];
@@ -12,15 +25,18 @@
         var timeNow = ('Today,' + new Date().getHours() + ':' + new Date().getMinutes());
         var smilesForChat = document.getElementById('smiles_for_chat');
         var contactListSearch = document.getElementById('contact_list_search');
-        chat.scrollTop = chat.scrollHeight;
+        var caret = document.getElementById('caret_bar');
+        var submitBtn = document.getElementById('submitBtn');
         var myData = {
             "_id": "507f191e810c19729de859ea",
             "name": "Eugene Gogol",
+            "link": "#",
             "thumb": "./img/eugene_thumb.png",
             "friends": [
                 {
                     "_id": "507f191e810c19729de860ea",
                     "name": "MaximModus",
+                    "link": "#",
                     "messages": [{
                         "text": "Hi!",
                         "date": "Yesterday, 16:28"
@@ -31,6 +47,7 @@
                 {
                     "_id": "507f191e810c19729de861ea",
                     "name": "Thomas Brown",
+                    "link": "#",
                     "messages": [],
                     "thumb": "./img/thomas_thumb.png",
                     "count": "3"
@@ -38,6 +55,7 @@
                 {
                     "_id": "507f191e810c19729de862ea",
                     "name": "Edward LeCriston",
+                    "link": "#",
                     "messages": [{
                         "text": "I agree, like!",
                         "date": "Yesterday, 18:53"
@@ -48,6 +66,7 @@
                 {
                     "_id": "507f191e810c19729de863ea",
                     "name": "Cristian Smitt",
+                    "link": "#",
                     "messages": [],
                     "thumb": "./img/cristian_thumb.png",
                     "count": "2"
@@ -55,6 +74,7 @@
                 {
                     "_id": "507f191e810c19729de864ea",
                     "name": "Sandy Miller",
+                    "link": "#",
                     "messages": [{
                         "text": "Who knows the recipe for stuffed cabbage ?",
                         "date": "Yesterday, 20:12"
@@ -64,6 +84,7 @@
                 {
                     "_id": "507f191e810c19729de865ea",
                     "name": "Princess Murphy",
+                    "link": "#",
                     "messages": [{
                         "text": "Hello, my friends! I love this pack of smiles",
                         "date": "Yesterday, 16:48"
@@ -73,6 +94,7 @@
                 {
                     "_id": "507f191e810c19729de866ea",
                     "name": "Artour Bargeman",
+                    "link": "#",
                     "messages": [{
                         "text": "Hello, Bogdan! Yes, funny smiles",
                         "date": "Yesterday, 17:00"
@@ -82,6 +104,7 @@
                 {
                     "_id": "507f191e810c19729de867ea",
                     "name": "Andrey Afonin",
+                    "link": "#",
                     "messages": [{
                         "text": "Privet bratishki",
                         "date": "10 minutes ago"
@@ -91,114 +114,154 @@
             ]
         };
 
+
         createContactList(myData.friends, contactList);
+
+        setScrollBarPositionBottom(chat);
+
+        caret.addEventListener('click', function (event) {
+
+            if (event.target.attributes.src.value === customData.caretImg[0]) {
+
+                event.target.attributes.src.value = customData.caretImg[1];
+
+                return;
+            }
+
+            event.target.attributes.src.value = customData.caretImg[0]
+        });
 
         contactListSearch.addEventListener('keyup', function (event) {
 
-            var regexp = event.target.value;
+            var inputTextRegexp = new RegExp(event.target.value.toLowerCase());
+
+            var hiddenBlock = customData.classes.hidden;
+
+            var hiddenOpacity = customData.classes.opacity;
 
             Array.prototype.forEach.call(contactListChildren, function (child) {
 
-                var contactName = child.getElementsByClassName('contact_name')[0];
+                var contactName = child.getElementsByClassName('contact_name')[0].innerHTML.toLowerCase();
 
-                if (!contactName.innerHTML.toLowerCase().match(regexp)) {
+                if (!inputTextRegexp.test(contactName)) {
 
-                    child.className = 'contact_user hidden';
+                    child.className = customData.classes.contactUser + hiddenOpacity;
+
+                    setTimeout(function () {
+
+                        child.className += hiddenBlock
+                    }, 500)
 
                 } else {
 
-                    child.className = 'contact_user';
+                    child.className = customData.classes.contactUser;
+
+                    if (!child.className.search(hiddenBlock)) {
+
+                        child.className = customData.classes.contactUser;
+                    }
                 }
             });
+        });
+
+        smilesForChat.addEventListener('click', function (event) {
+
+            textarea.appendChild(createEl({el: 'img', elClass: customData.classes.smileImg, attrs: [['src', event.target.attributes.src.value]]}))
         });
 
         contactList.addEventListener('click', function (event) {
 
             Array.prototype.forEach.call(contactListChildren, function (child) {
 
-                if (event.target.tagName === 'LI') {
-
-                    event.target.className += ' active';
-                }
-
-                child.className = "contact_user";
+                child.className = customData.classes.contactUser;
             });
+
+            if (event.target.tagName === 'LI') {
+
+                event.target.className += customData.classes.active;
+            }
+
         });
 
         menu.addEventListener('click', function () {
             var inc = 0;
 
-            if (!menuNavigation.style.opacity) {
+            if (!navMnu.style.opacity) {
 
-                menuNavigation.style.opacity = 1;
+                navMnu.style.opacity = 1;
 
-                Array.prototype.forEach.call(children, function (child) {
+                navMnuRecurse(navMnuChildren, inc);
 
-                    child.style.top = inc + 'px';
+                navMnu.style.left = 25 + 'px';
 
-                    inc += 30;
-                });
-
-                return menuNavigation.style.left = 25 + 'px';
+                return;
             }
 
-            Array.prototype.forEach.call(children, function (child) {
+            navMnuRecurse(navMnuChildren);
 
-                child.style.top = 0;
-            });
-
-            menuNavigation.style.opacity = '';
+            navMnu.style.opacity = '';
         });
         /*TODO: 1.Fix P Tag (all text inline);
          2.Prevent sending empty messages;*/
 
         document.body.addEventListener('mouseup', function () {
-            if (menuNavigation.style.opacity) {
 
-                Array.prototype.forEach.call(children, function (child) {
+            if (navMnu.style.opacity) {
 
-                    child.style.top = 0;
+                Array.prototype.forEach.call(navMnuChildren, function (child) {
+
+                    child.style.top = -100 +'px';
 
                 });
 
-                menuNavigation.style.opacity = '';
+                navMnu.style.opacity = '';
+            }
+            if (!smilesForChat.className) {
+                smilesForChat.className += customData.classes.hidden;
             }
         });
 
         textarea.addEventListener('keyup', function (event) {
-            if (event.ctrlKey && event.keyCode === 13 && event.target.value) {
+
+            if (event.ctrlKey && event.keyCode === 13) {
 
                 event.preventDefault();
 
                 chatWindow.appendChild(createEl({el: 'li', children: [
                                             createEl({el: 'div', elClass: 'img_wrap', children: [
-                                                createEl({el: 'img', elClass:'thumb', attrs: [['src', myData.thumb]]})
+                                                createEl({el: 'a', attrs: [['href', myData.link], ['target', '_blank']], children: [
+                                                    createEl({el: 'img', elClass:'thumb', attrs: [['src', myData.thumb]]})
+                                                ]})
                                             ]}),
                                             createEl({el: 'div', elClass: 'text_wrap', children: [
                                                 createEl({el: 'p', elClass: 'chat_name', innerData: myData.name}),
                                                 createEl({el: 'p', elClass: 'chat_message_data', innerData: timeNow}),
                                                 createEl({el: 'div', elClass: 'chat_message_wrap', children: [
-                                                    createEl({el: 'p', innerData: event.target.value})
+                                                    createEl({el: 'div', innerData: event.target.innerHTML})
                                                 ]})
                                             ]})
                     ]}));
 
                 chat.scrollTop = chat.scrollHeight;
 
-                event.target.value = '';
+                event.target.innerHTML = '';
 
                 return;
             }
             if (event.keyCode === 13) {
 
-                event.target.value += '\n';
+                event.target.scrollTop = event.target.scrollHeight;
+
+                event.target.innerHTML += '\n';
+
             }
         });
 
         chatSmiles.addEventListener('click', function () {
+
             if (!smilesForChat.className) {
 
-                smilesForChat.className = "hidden";
+                smilesForChat.className += customData.classes.hidden;
 
                 return;
             }
@@ -208,8 +271,9 @@
     });
     
     function createEl (elObj) {
+
         var node = document.createElement(elObj.el);
-        
+
         if (elObj.elClass) {
             node.className = elObj.elClass;
         }
@@ -223,6 +287,7 @@
                 node.setAttribute(elObj.attrs[j][0], elObj.attrs[j][1]);
             }
         }
+
         if (elObj.children) {
             for (var i = 0; i < elObj.children.length; i++) {
                 node.appendChild(elObj.children[i]);
@@ -233,7 +298,9 @@
     }
 
     function createContactList(list, parent) {
+
         return list.forEach(function (contact) {
+
             parent.appendChild(createEl({el: 'li', elClass: 'contact_user', children: [
                                     createEl({el: 'img', attrs: [['src', contact.thumb]]}),
                                     createEl({el: 'p', elClass: 'contact_name', innerData: contact.name}),
@@ -241,6 +308,43 @@
                                         createEl({el: 'p', innerData: contact.count})
                                     ]})
                 ]}));
-    })
+
+            var contactCount = document.getElementsByClassName('contact_count');
+
+            Array.prototype.forEach.call(contactCount, function (el) {
+
+                var pCount = el.getElementsByTagName('p')[0];
+
+                if (pCount.innerHTML > 99) {
+
+                    pCount.innerHTML = '99+';
+
+                } else if (!pCount.innerHTML) {
+
+                    pCount.className += customData.classes.hidden;
+                }
+            });
+        })
+    }
+
+
+    function navMnuRecurse(node, inc) {
+
+        Array.prototype.forEach.call(node, function (child) {
+
+            if (child.children) {
+
+                navMnuRecurse(child.children);
+            }
+
+            child.style.top = inc + 'px';
+
+            inc += 30;
+        });
+    }
+
+    function setScrollBarPositionBottom(selector) {
+
+        selector.scrollTop = selector.scrollHeight;
     }
 })();
